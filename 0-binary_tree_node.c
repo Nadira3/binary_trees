3b060d32b35fec9701afa542e3a722aac69f4de0
Nadira3 <paitanun35@gmail.com>
2023-11-01 18:41:34 +0100

    betty styling fix

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,17 +1,24 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_node - creates a node
+ * @parent: node's parent
+ * @value: node->n
+ * Return: node
+ */
+
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node;
 
 	node = malloc(sizeof(binary_tree_t));
 	if (!node)
-		return(0);
+		return (0);
 
 	node->n = value;
 	node->parent = parent;
 	node->left = NULL;
 	node->right = NULL;
 
-	return(node);
+	return (node);
 }
