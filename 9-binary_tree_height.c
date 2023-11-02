e8d1b919cc65e277afb50a99c12f9cd3178d2171
Nadira3 <paitanun35@gmail.com>
2023-11-02 08:40:06 +0100

    ?corrected binary_tree height implementation

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,21 +1,5 @@
 #include "binary_trees.h"
 
-size_t find_tree_height(const binary_tree_t *tree, size_t num)
-{
-	if (tree && !tree->left && !tree->right)
-		return (num);
-	if (tree)
-	{
-		if (tree->left && tree->right)
-			return find_tree_height(tree->left, num + 1);
-		if (tree->left && !tree->right)
-			return find_tree_height(tree->left, num + 1);
-		if (!tree->left && tree->right)
-			return find_tree_height(tree->right, num + 1);
-	}
-	return (num);
-}
-
 /**
  * Write a function that measures the height of a binary tree
  *
@@ -26,5 +10,13 @@ size_t find_tree_height(const binary_tree_t *tree, size_t num)
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	return (find_tree_height(tree, 0));
+	size_t height_l = 0;
+	size_t height_r = 0;
+
+	if (tree)
+	{
+		height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	}
+	return (height_l > height_r ? height_l : height_r);
 }
