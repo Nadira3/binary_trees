df073611efa3e48df93dc7476a72121cb69818f6
Nadira3 <paitanun35@gmail.com>
2023-11-02 08:39:42 +0100

    ?corrected binary_tree size implementation

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,4 +1,15 @@
 #include "binary_trees.h"
+size_t find_tree_size(const binary_tree_t *tree)
+{
+	size_t size_l = 0, size_r = 0;
+
+	if (tree)
+	{
+		size_l = tree->left ? 1 + find_tree_size(tree->left) : 0;
+		size_r = tree->right ? 1 + find_tree_size(tree->right) : 0;
+	}
+	return (size_l + size_r);
+}
 
 /**
  * Write a function that measures the size of a binary tree
@@ -8,27 +19,8 @@
  * If tree is NULL, the function must return 0
  */
 
-size_t find_tree_size(const binary_tree_t *tree, size_t num)
-{
-	if (tree && ((num && !tree->parent) || (!tree->left && !tree->right)))
-		return (num + 1);
-	if (tree)
-	{
-		if (tree->left)
-			return find_tree_size(tree->left, num + 1);
-		if (tree->right)
-			return find_tree_size(tree->right, num + 1);
-	}
-	return (num);
-}
-
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	/* size of tree  size of left subtree + size of right subtree + 1(node) */
-	size_t result = 0;
-	
-	if (tree)
-		result = find_tree_size(tree->left, 0) + find_tree_size(tree->right, 0) + 1;
-
-	return (result);
+	/* size of tree = size of left subtree + size of right subtree + 1(node) */
+	return (find_tree_size(tree) + 1);
 }
