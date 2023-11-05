c22e2ce8cf546bc7c2c5c4ac19da4365c9ce3fde
Nadira3 <paitanun35@gmail.com>
2023-11-05 18:41:52 +0100

    proper implementation of is_bst function that recursively checks each subtree is a valid bst

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -1,26 +1,64 @@
 #include "binary_trees.h"
-#include "9-binary_tree_height.c"
 
 /**
- * Write a function that is_bsts if a binary tree is a valid Binary Search Tree
+ * is_bst_helper_left - checks if a node's left side is a valid BST
+ *
+ * @tree: is a pointer to the root node of the tree to is_bst
+ * @max: node value
+ * Return: 1 if tree is a valid BST, and 0 otherwise
+ */
+int is_bst_helper_left(const binary_tree_t *tree, int max)
+{
+	if (!tree)
+		return (1);
+	if (tree->n > max)
+		return (0);
+	return (is_bst_helper_left(tree->left, max) &&
+			is_bst_helper_left(tree->right, max));
+	return (1);
+}
+
+/**
+ * is_bst_helper_right - checks if a node's right side is a valid BST
+ *
+ * @tree: is a pointer to the root node of the tree to is_bst
+ * @min: node value
+ * Return: 1 if tree is a valid BST, and 0 otherwise
+ */
+int is_bst_helper_right(const binary_tree_t *tree, int min)
+{
+	if (!tree)
+		return (1);
+	if (tree->n < min)
+		return (0);
+	return (is_bst_helper_right(tree->left, min) &&
+			is_bst_helper_right(tree->right, min));
+	return (1);
+}
+
+/**
+ * binary_tree_is_bst - checks if a binary tree is a valid Binary Search Tree
  *
  * Prototype: int binary_tree_is_bst(const binary_tree_t *tree);
- * Where tree is a pointer to the root node of the tree to is_bst
- * Your function must return 1 if tree is a valid BST, and 0 otherwise
+ * @tree: is a pointer to the root node of the tree to is_bst
+ * Return: 1 if tree is a valid BST, and 0 otherwise
  * If tree is NULL, return 0
  *
  * Properties of a Binary Search Tree:
  *
- * The left subtree of a node contains only nodes with values less than the node’s value
- * The right subtree of a node contains only nodes with values greater than the node’s value
+ * The left subtree of a node contains only nodes with values < node’s value
+ * The right subtree of a node contains only nodes with values > node’s value
  * The left and right subtree each must also be a binary search tree
  * There must be no duplicate values
  */
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
-	if (tree && tree->left->n < tree->n && tree->right->n > tree->n)
+	if (tree && (tree->left && tree->right ? (tree->left->n < tree->n &&
+					tree->right->n) > tree->n : tree->left ?
+				tree->left->n < tree->n : tree->right ? tree->right->n > tree->n : 1))
 		return (1);
-	if (binary_tree_height(tree) > 1)
+	if (tree && is_bst_helper_left(tree->left, tree->n) &&
+			is_bst_helper_right(tree->right, tree->n))
 		return (binary_tree_is_bst(tree->left) && binary_tree_is_bst(tree->right));
 	return (0);
 }
