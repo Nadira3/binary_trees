552804cb51b208845f080ff4bd6fb5ba01c9b317
Nadira3 <paitanun35@gmail.com>
2023-11-07 12:44:33 +0100

    is_complete function correction

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -13,9 +13,8 @@ int binary_tree_is_complete(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (1);
-	if (binary_tree_is_full(tree) || (!binary_tree_is_full((binary_tree_t *)tree) && 
-			binary_tree_is_full(binary_tree_sibling((binary_tree_t *)tree))))
-		if (tree->left && binary_tree_is_complete(tree->right) && binary_tree_is_complete(tree->left))
+	if (binary_tree_is_full(tree) || (!binary_tree_is_full(tree) && (tree->parent ? binary_tree_is_full(binary_tree_sibling((binary_tree_t *)tree)) : binary_tree_is_root(tree)) && tree->left))
+		if (binary_tree_is_complete(tree->right) && binary_tree_is_complete(tree->left))
 			return (1);
 	return (0);
 }
